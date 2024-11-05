80f33d3caa832a781ed45a20114879366be9b843
Casey Mulcahy <caseymulcahy@proton.me>
2024-11-05 15:06:34 -0500

    Live config of MIDI input

--- src/main.rs
+++ src/main.rs
@@ -3,58 +3,110 @@
 
 use std::sync::mpsc::{channel, Sender, Receiver};
 use std::error::Error;
+use std::collections::VecDeque;
 
-use midir::{MidiInput, MidiInputConnection, MidiInputPort};
+use midir::{InitError, MidiInput, MidiInputConnection, MidiInputPort};
 use cpal::{traits::{DeviceTrait, HostTrait, StreamTrait}, StreamConfig};
 use fundsp::hacker::*;
 use eframe::egui;
+use anyhow::{bail, Result};
 
 pub mod pitch;
 pub mod input;
 
 const APP_NAME: &str = "Synth Tracker";
 
+struct MessageBuffer {
+    capacity: usize,
+    messages: VecDeque<String>,
+}
+
+impl MessageBuffer {
+    fn new(capacity: usize) -> Self {
+        MessageBuffer {
+            capacity,
+            messages: VecDeque::new(),
+        }
+    }
+
+    fn push(&mut self, msg: String) {
+        self.messages.push_front(msg);
+        self.messages.truncate(self.capacity);
+    }
+
+    fn report(&mut self, e: &impl std::fmt::Display) {
+        self.push(format!("{}", e));
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &'_ String> {
+        self.messages.iter().rev()
+    }
+}
+
+struct Midi {
+    input: Option<MidiInput>,
+    port_name: Option<String>,
+    port_selection: Option<String>,
+    conn: Option<MidiInputConnection<Sender<Vec<u8>>>>,
+    rx: Option<Receiver<Vec<u8>>>,
+    input_id: u16,
+}
+
+impl Midi {
+    fn new() -> Self {
+        let mut m = Self {
+            input: None,
+            port_name: None,
+            port_selection: None,
+            conn: None,
+            rx: None,
+            input_id: 0,
+        };
+        m.input = m.new_input().ok();
+        m
+    }
+
+    fn new_input(&mut self) -> Result<MidiInput, InitError> {
+        self.input_id += 1;
+        MidiInput::new(&format!("{} input #{}", APP_NAME, self.input_id))
+    }
+
+    fn selected_port(&self) -> Option<MidiInputPort> {
+        self.port_selection.as_ref().map(|selection| {
+            self.input.as_ref().map(|input| {
+                for port in input.ports() {
+                    if let Ok(name) = input.port_name(&port) {
+                        if name == *selection {
+                            return Some(port)
+                        }
+                    }
+                }
+                None
+            })?
+        })?
+    }
+}
+
 struct App {
     tuning: pitch::Tuning,
-    messages: Vec<String>,
+    messages: MessageBuffer,
     f: Shared,
     gate: Shared,
     octave: i8,
-    midi_in: Option<MidiInput>,
-    midi_port: Option<MidiInputPort>,
-    midi_conn: Option<MidiInputConnection<Sender<Vec<u8>>>>,
-    midi_rx: Option<Receiver<Vec<u8>>>,
-    midi_input_id: u16,
+    midi: Midi,
 }
 
 impl App {
     fn new(f: Shared, gate: Shared) -> Self {
         let mut app = App {
             tuning: pitch::Tuning::divide(2.0, 12, 1).unwrap(),
-            messages: vec![],
+            messages: MessageBuffer::new(100),
             f,
             gate,
             octave: 4,
-            midi_in: None,
-            midi_port: None,
-            midi_conn: None,
-            midi_rx: None,
-            midi_input_id: 0,
+            midi: Midi::new(),
         };
-        app.open_midi_input();
         app
-    }
-
-    fn open_midi_input(&mut self) {
-        self.midi_input_id += 1;
-        match MidiInput::new(&format!("{} input #{}", APP_NAME, self.midi_input_id)) {
-            Ok(input) => self.midi_in = Some(input),
-            Err(e) => self.report(&e),
-        }
-    }
-
-    fn report(&mut self, e: &impl std::fmt::Display) {
-        self.messages.push(format!("{}", e))
     }
 
     fn handle_ui_event(&mut self, evt: &egui::Event) {
@@ -63,7 +115,7 @@ impl App {
                 if let Some(key) = physical_key {
                     if let Some(note) = input::note_from_key(key, &self.tuning, self.octave) {
                         if *pressed {
-                            self.messages.push(format!("{}", &note));
+                            self.messages.report(&note);
                             self.f.set(midi_hz(self.tuning.midi_pitch(&note)));
                             self.gate.set(1.0);
                         } else {
@@ -77,21 +129,28 @@ impl App {
         }
     }
 
-    fn midi_connect(&mut self, ctx: egui::Context) -> Result<MidiInputConnection<Sender<Vec<u8>>>, Box<dyn Error>> {
-        self.midi_input_id += 1;
-        let midi_in = MidiInput::new(&format!("{} input #{}", APP_NAME, self.midi_input_id))?;
-
-        let (tx, rx) = channel();
-        self.midi_rx = Some(rx);
-        Ok(midi_in.connect(
-            &self.midi_port.as_ref().unwrap(),
-            APP_NAME,
-            move |_, message, tx| {
-                tx.send(message.to_vec());
-                ctx.request_repaint();
+    fn midi_connect(&mut self, ctx: egui::Context) -> Result<MidiInputConnection<Sender<Vec<u8>>>> {
+        match self.midi.selected_port() {
+            Some(port) => {
+                match self.midi.new_input() {
+                    Ok(input) => {
+                        let (tx, rx) = channel();
+                        self.midi.rx = Some(rx);
+                        Ok(input.connect(
+                            &port,
+                            APP_NAME,
+                            move |_, message, tx| {
+                                tx.send(message.to_vec());
+                                ctx.request_repaint();
+                            },
+                            tx,
+                        )?)
+                    },
+                    Err(e) => bail!(e),
+                }
             },
-            tx,
-        )?)
+            None => bail!("no MIDI port selected")
+        }
     }
 
     fn handle_midi(&self, message: &[u8]) {
@@ -113,50 +172,63 @@ impl App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            if self.midi_conn.is_none() {
-                if let Some(midi_in) = &self.midi_in {
-                    egui::ComboBox::from_label("Select MIDI input port")
-                        .selected_text(match &self.midi_port {
-                            Some(p) => midi_in.port_name(&p).unwrap_or(String::from("")),
-                            None => String::from(""),
-                        })
-                        .show_ui(ui, |ui| {
-                            for p in midi_in.ports() {
-                                let name = midi_in.port_name(&p).unwrap_or(String::from(""));
-                                ui.selectable_value(&mut self.midi_port, Some(p), name);
-                            }
-                        });
-                    if self.midi_port.is_some() {
-                        match self.midi_connect(ctx.clone()) {
-                            Ok(conn) => {
-                                self.midi_conn = Some(conn);
-                                self.report(&"Connected to MIDI input port");
-                            },
-                            Err(e) => self.report(&e),
+        // process UI input
+        ctx.input(|input| {
+            for evt in input.events.iter() {
+                self.handle_ui_event(evt);
+            }
+        });
+
+        // process MIDI input
+        if let Some(rx) = &self.midi.rx {
+            loop {
+                match rx.try_recv() {
+                    Ok(v) => {
+                        self.handle_midi(&v);
+                        self.messages.push(format!("Received MIDI message: {:?}", &v));
+                    },
+                    Err(_) => break,
+                }
+            }
+        }
+
+        // bottom panel
+        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+            if self.midi.input.is_some() {
+                egui::ComboBox::from_label("MIDI input port")
+                    .selected_text(self.midi.port_name.clone().unwrap_or("".to_string()))
+                    .show_ui(ui, |ui| {
+                        let input = self.midi.input.as_ref().unwrap();
+                        for p in input.ports() {
+                            let name = input.port_name(&p).unwrap_or(String::from(""));
+                            ui.selectable_value(&mut self.midi.port_selection, Some(name.clone()), name);
                         }
+                    });
+                if self.midi.port_selection.is_some() && self.midi.port_selection != self.midi.port_name {
+                    match self.midi_connect(ctx.clone()) {
+                        Ok(conn) => {
+                            let old_conn = std::mem::replace(&mut self.midi.conn, Some(conn));
+                            if let Some(c) = old_conn {
+                                c.close();
+                            }
+                            self.midi.port_name = self.midi.port_selection.clone();
+                            self.midi.port_name.as_ref().inspect(|name| {
+                                self.messages.push(format!("Connected to {} for MIDI input", name));
+                            });
+                        },
+                        Err(e) => self.messages.report(&e),
                     }
                 }
             }
-            for line in self.messages.iter() {
-                ui.label(line);
-            }
-            ctx.input(|input| {
-                for evt in input.events.iter() {
-                    self.handle_ui_event(evt);
+        });
+
+        // message panel
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for line in self.messages.iter() {
+                    ui.label(line);
                 }
             });
-            if let Some(rx) = &self.midi_rx {
-                loop {
-                    match rx.try_recv() {
-                        Ok(v) => {
-                            self.handle_midi(&v);
-                            self.messages.push(format!("Received MIDI message: {:?}", &v));
-                        },
-                        Err(_) => break,
-                    }
-                }
-            }
         });
     }
 }
@@ -198,7 +270,7 @@ fn main() -> eframe::Result {
     stream.play().unwrap();
 
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
         ..Default::default()
     };
     eframe::run_native(
