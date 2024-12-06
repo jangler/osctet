9131550f220a0113ba6b3962f2d83c8697eb9497
Casey Mulcahy <caseymulcahy@proton.me>
2024-12-05 20:22:39 -0500

    Write panic location to error file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // disable console in windows release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::error::Error;
+use std::{error::Error, fs, panic};
 
 use macroquad::prelude::Conf;
 
 use osctet::{APP_NAME, run};
+
+const PANIC_FILE: &str = "error.txt";
 
 fn window_conf() -> Conf {
     Conf {
@@ -19,5 +21,15 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() -> Result<(), Box<dyn Error>> {
+    panic::set_hook(Box::new(|info| {
+        let message = if let Some(location) = info.location() {
+            &format!("panic at {}:{}:{}\n",
+                location.file(), location.line(), location.column())
+        } else {
+            "panic at unknown location"
+        };
+        let _ = fs::write(PANIC_FILE, message);
+        eprint!("{}", message);
+    }));
     run().await
 }
