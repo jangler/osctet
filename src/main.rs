b252420dec64eb8a3f9c30c809a254565f4712d4
Casey Mulcahy <caseymulcahy@proton.me>
2024-12-10 10:17:02 -0500

    Only set panic hook in release mode

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,18 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // panic::set_hook(Box::new(|info| {
-    //     let message = if let Some(location) = info.location() {
-    //         &format!("panic at {}:{}:{}\n",
-    //             location.file(), location.line(), location.column())
-    //     } else {
-    //         "panic at unknown location"
-    //     };
-    //     let _ = fs::write(PANIC_FILE, message);
-    //     eprint!("{}", message);
-    // }));
+    if cfg!(not(debug_assertions)) {
+        panic::set_hook(Box::new(|info| {
+            let message = if let Some(location) = info.location() {
+                &format!("panic at {}:{}:{}\n",
+                    location.file(), location.line(), location.column())
+            } else {
+                "panic at unknown location"
+            };
+            let _ = fs::write(PANIC_FILE, message);
+            eprint!("{}", message);
+        }));
+    }
+    
     run().await
 }
