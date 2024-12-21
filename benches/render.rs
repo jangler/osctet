7283352e8b86ff04e9652c48ba27cd0d417d75e5
Casey Mulcahy <caseymulcahy@proton.me>
2024-12-21 11:04:51 -0500

    Add interpolation benchmark

--- benches/render.rs
+++ benches/render.rs
@@ -2,15 +2,23 @@ use std::{hint::black_box, path::PathBuf};
 use criterion::{criterion_group, criterion_main, Criterion};
 use osctet::{module::Module, playback::render};
 
-fn scale(c: &mut Criterion) {
-    let path = PathBuf::from("./testdata/scale.osctet");
+fn render_module(c: &mut Criterion, filename: &str) {
+    let path: PathBuf = ["./testdata", filename].iter().collect();
     let module = Module::load(&path).unwrap();
-    c.bench_function("render",
+    c.bench_function(&format!("render {}", filename),
         |b| b.iter(|| black_box({
             let rx = render(module.clone(), path.clone());
             while let Ok(_) = rx.recv() {}
         })));
 }
 
-criterion_group!(benches, scale);
+fn scale(c: &mut Criterion) {
+    render_module(c, "scale.osctet");
+}
+
+fn interpolation(c: &mut Criterion) {
+    render_module(c, "interpolation.osctet");
+}
+
+criterion_group!(benches, scale, interpolation);
 criterion_main!(benches);
