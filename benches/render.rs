c21109b8f43f2e392ae6e45e746d3d5b307bad4c
Casey Mulcahy <caseymulcahy@proton.me>
2024-12-20 20:25:29 -0500

    Update render benchmark

--- benches/render.rs
+++ benches/render.rs
@@ -1,20 +1,16 @@
 use std::{hint::black_box, path::PathBuf};
 use criterion::{criterion_group, criterion_main, Criterion};
-use osctet::{module::{Event, EventData, Module}, playback::render};
+use osctet::{module::Module, playback::render};
 
-fn empty_module(c: &mut Criterion) {
-    let mut module = Module::new(Default::default());
-    module.tracks[0].channels[0].events.push(Event {
-        tick: 480,
-        data: EventData::End,
-    });
-    let path = PathBuf::default();
-    // TODO: module cloning is probably costly here!
+fn scale(c: &mut Criterion) {
+    let path = PathBuf::from("./testdata/scale.osctet");
+    let module = Module::load(&path).unwrap();
     c.bench_function("render",
-        |b| b.iter(|| black_box(render(module.clone(), path.clone()))));
+        |b| b.iter(|| black_box({
+            let rx = render(module.clone(), path.clone());
+            while let Ok(_) = rx.recv() {}
+        })));
 }
 
-// TODO: benchmark doing more DSP
-
-criterion_group!(benches, empty_module);
+criterion_group!(benches, scale);
 criterion_main!(benches);
